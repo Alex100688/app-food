a881ee0deabca140270c9d28abed08d37e16a58d
Alex100688 <alessiozingaro2@gmail.com>
2023-04-17 17:19:54 +0200

    Popolata la tabella app_food

--- database/migrations/2023_04_17_142402_create_app_foods_table.php
+++ database/migrations/2023_04_17_142402_create_app_foods_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('app_foods', function (Blueprint $table) {
             $table->id();
+            $table->string('name_food', 30) -> unique();
+            $table->string('name_drink', 30) -> unique();
+            $table->text('description', 1000);
+            $table->string('address', 200);
+            $table->string('phone_number', 40);
+            $table->string('photo_food') -> nullable();
             $table->timestamps();
         });
     }
